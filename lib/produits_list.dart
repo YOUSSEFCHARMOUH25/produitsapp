fd13b29b91e15e0576a88daaae4f767c4fa7adaf
youssefcharmouh <charmouhyoussef25@gmail.com>
2024-10-22 20:21:24 +0100

    ajouter la déclaration d'une list de produits dans la class ProduitsList ,ajouter un widget ListView

--- lib/produits_list.dart
+++ lib/produits_list.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:produitsapp/produit_box.dart';
 
 class ProduitsList extends StatelessWidget {
-  const ProduitsList({super.key});
+  final List<Map<String, dynamic>> liste = [
+    {"nom": "1 Produit", "selectionne": false},
+    {"nom": "2 Produit", "selectionne": true},
+    {"nom": "3 Produit", "selectionne": false},
+    {"nom": "4 Produit", "selectionne": false},
+    {"nom": "5 Produit", "selectionne": false},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Produits"),
+      ),
+      body: ListView.builder(
+        itemCount: liste.length,
+        itemBuilder: (context, index) {
+          return ProduitBox(nomProduit: liste[index][0],
+          );
+        },
+      ),
+    );
   }
 }
