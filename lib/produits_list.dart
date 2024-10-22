fb0311e615ea2c921c18bcd345a47b16bdbbb5a8
youssefcharmouh <charmouhyoussef25@gmail.com>
2024-10-22 19:52:43 +0100

    Ajouter le widget ProduitsList

---
+++ lib/produits_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProduitsList extends StatelessWidget {
+  const ProduitsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
